82bd38d4e266cbadbb8cea3f2f73310c00df1b33
Lakshan211 <139627654+Lakshan211@users.noreply.github.com>
2024-12-10 13:23:51 +0000

    chages

--- backend/signup.php
+++ backend/signup.php
@@ -21,7 +21,7 @@
 	}
 	
 	if ($_SERVER['REQUEST_METHOD'] == "POST") {
-		$username = $_POST['name'];
+		$name = $_POST['name'];
 		$email = $_POST['email'];
 		$password = $_POST['password'];
 		AddData($connect,$name,$email,$password);
