2e39c810a813f298e8dffe743ac3ecde74465adb
Lakshan211 <139627654+Lakshan211@users.noreply.github.com>
2024-12-10 08:04:58 +0000

    some changes

--- backend/signup.php
+++ backend/signup.php
@@ -1,109 +1,30 @@
 <?php
-require 'vendor/autoload.php';
+	require_once 'dbconf.php';
+	function AddData($connect,$name,$email,$password){
+		try {
+            $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+            
+		
+			$sql = "INSERT INTO user VALUES('$username','$hashed_password')";
+			
+			$result = mysqli_query($connect,$sql);
+			if ($result) {
+				// echo "create Account sucessfully";
+			} else {
+				die("Error ".mysqli_error($connect));
+			}
+            header('Location: ../form.html');
+            exit;
+		} catch (Exception $e) {
+			die($e->getMessage());
+		}
+	}
+	
+	if ($_SERVER['REQUEST_METHOD'] == "POST") {
+		$username = $_POST['username'];
+		$password = $_POST['password_has'];
+		AddData($connect,$username,$password);
+	}
+	
 
-use PhpOffice\PhpSpreadsheet\Spreadsheet;
-use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $confirm_password = $_POST['confirm_password'];
-
-
-    if ($password !== $confirm_password) {
-        echo "Passwords do not match.";
-        exit;
-    }
-
-    $file = 'users.xlsx';
-    $spreadsheet = file_exists($file) ? \PhpOffice\PhpSpreadsheet\IOFactory::load($file) : new Spreadsheet();
-    $sheet = $spreadsheet->getActiveSheet();
-
-    $userExists = false;
-    foreach ($sheet->getRowIterator() as $row) {
-        $cellIterator = $row->getCellIterator();
-        $cellIterator->setIterateOnlyExistingCells(false);
-        $rowData = iterator_to_array($cellIterator);
-        
-        if (isset($rowData[0]) && $rowData[0] == $name) {
-            $userExists = true;
-            break;
-        }
-        if (isset($rowData[1]) && $rowData[1] == $email) {
-            $userExists = true;
-            break;
-        }
-    }
-
-    if ($userExists) {
-        echo "Username or Email already exists.";
-        exit;
-    }
-
-    $lastRow = $sheet->getHighestRow() + 1;
-    $sheet->setCellValue('A' . $lastRow, $name);
-    $sheet->setCellValue('B' . $lastRow, $email);
-    $sheet->setCellValue('C' . $lastRow, password_hash($password, PASSWORD_DEFAULT));
-
-    $writer = new Xlsx($spreadsheet);
-    $writer->save($file);
-
-    echo "Signup successful! You can now <a href='book.html'>login</a>";
-}
-?>
-=======
-<?php
-require 'vendor/autoload.php';
-
-use PhpOffice\PhpSpreadsheet\Spreadsheet;
-use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $confirm_password = $_POST['confirm_password'];
-
-
-    if ($password !== $confirm_password) {
-        echo "Passwords do not match.";
-        exit;
-    }
-
-    $file = 'users.xlsx';
-    $spreadsheet = file_exists($file) ? \PhpOffice\PhpSpreadsheet\IOFactory::load($file) : new Spreadsheet();
-    $sheet = $spreadsheet->getActiveSheet();
-
-    $userExists = false;
-    foreach ($sheet->getRowIterator() as $row) {
-        $cellIterator = $row->getCellIterator();
-        $cellIterator->setIterateOnlyExistingCells(false);
-        $rowData = iterator_to_array($cellIterator);
-        
-        if (isset($rowData[0]) && $rowData[0] == $name) {
-            $userExists = true;
-            break;
-        }
-        if (isset($rowData[1]) && $rowData[1] == $email) {
-            $userExists = true;
-            break;
-        }
-    }
-
-    if ($userExists) {
-        echo "Username or Email already exists.";
-        exit;
-    }
-
-    $lastRow = $sheet->getHighestRow() + 1;
-    $sheet->setCellValue('A' . $lastRow, $name);
-    $sheet->setCellValue('B' . $lastRow, $email);
-    $sheet->setCellValue('C' . $lastRow, password_hash($password, PASSWORD_DEFAULT));
-
-    $writer = new Xlsx($spreadsheet);
-    $writer->save($file);
-
-    echo "Signup successful! You can now <a href='book.html'>login</a>";
-}
-?>
+	?>
