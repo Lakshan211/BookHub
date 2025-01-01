5fc92b8c9350189601159fd3854a29b88ecca29a
Lakshan211 <139627654+Lakshan211@users.noreply.github.com>
2025-01-01 16:49:11 +0000

    update contact

---
+++ backend/contact.php
@@ -0,0 +1,31 @@
+<?php
+	require_once 'dbconfic.php';
+	function AddData($connect,$name,$email,$subject,$message){
+		try {
+            
+		
+			$sql = "INSERT INTO user VALUES('$name','$email','$subject', '$message')";
+			
+			$result = mysqli_query($connect,$sql);
+			if ($result) {
+				// echo "create Account sucessfully";
+			} else {
+				die("Error ".mysqli_error($connect));
+			}
+            header('Location: ../index.html');
+            exit;
+		} catch (Exception $e) {
+			die($e->getMessage());
+		}
+	}
+	
+	if ($_SERVER['REQUEST_METHOD'] == "POST") {
+		$name = $_POST['name'];
+		$email = $_POST['email'];
+		$subject = $_POST['subject'];
+        $message=$_POST['message'];
+		AddData($connect,$name,$email,$subject, $message);
+	}
+	
+
+	?>
